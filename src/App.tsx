dd961425a9898ebd2fdf6c7ac508290a02430384
HugoLaurent <lauhugopro@gmail.com>
2023-07-13 18:42:59 +0200

    Add: animation between page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './container/Header/Header';
 import Home from './pages/Home/Home';
 import Article from './pages/Article/Article';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css'; // Assurez-vous d'importer votre fichier CSS contenant les styles de transition
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/article" element={<Article />} />
-      </Routes>
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/article" element={<Article />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
     </>
   );
 }
