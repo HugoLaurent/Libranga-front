bfcfd931f3267df60b8824708f65874c9eefff0f
HugoLaurent <lauhugopro@gmail.com>
2023-07-13 16:18:18 +0200

    add: fectCategroyWithArticle

--- src/App/reducers/categoryReducer.ts
+++ src/App/reducers/categoryReducer.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk, createReducer } from '@reduxjs/toolkit';
 
 export const initialState = {
   categories: [],
+  categoryWithArticle: [],
 };
 
 export const fetchCategory = createAsyncThunk(
@@ -13,9 +14,24 @@ export const fetchCategory = createAsyncThunk(
   }
 );
 
+export const fetchCategoryWithArticle = createAsyncThunk(
+  'category/fetchCategoryWithArticle',
+  async (id) => {
+    const response = await axios.get(
+      `http://localhost:3500/api/category/${id}`
+    );
+    console.log(response.data);
+
+    return response.data;
+  }
+);
+
 const categoryReducer = createReducer(initialState, (builder) => {
   builder.addCase(fetchCategory.fulfilled, (state, action) => {
     state.categories = action.payload;
+  });
+  builder.addCase(fetchCategoryWithArticle.fulfilled, (state, action) => {
+    state.categoryWithArticle = action.payload;
   });
 });
 
